451116b7072d6054578dad4b24d2f4c56c276aaa
Anton <antonmcde@icloud.com>
2019-01-16 15:38:50 +0200

    TableView Added

---
+++ Booking and Invoicing for Trainers/Booking and Invoicing for Trainers/ClientsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Booking and Invoicing for Trainers
+//
+//  Created by Anton Huisamen on 2019/01/16.
+//  Copyright © 2019 antonsolo. All rights reserved.
+//
+
+import UIKit
+
+class ClientsViewController: UIViewController {
+    
+    @IBOutlet weak var clientListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Booking and Invoicing for Trainers/Booking and Invoicing for TrainersTests/Booking_and_Invoicing_for_TrainersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Booking_and_Invoicing_for_TrainersTests.swift
+//  Booking and Invoicing for TrainersTests
+//
+//  Created by Anton Huisamen on 2019/01/16.
+//  Copyright © 2019 antonsolo. All rights reserved.
+//
+
+import XCTest
+@testable import Booking_and_Invoicing_for_Trainers
+
+class Booking_and_Invoicing_for_TrainersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
