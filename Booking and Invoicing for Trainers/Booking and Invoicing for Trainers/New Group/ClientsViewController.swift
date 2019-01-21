29f3b76e018213933f2fc8433f2b81daba069b0f
Anton <antonmcde@icloud.com>
2019-01-21 15:51:28 +0200

    Tableview working with alertcontroller name append function

--- Booking and Invoicing for Trainers/Booking and Invoicing for Trainers/New Group/ClientsViewController.swift
+++ Booking and Invoicing for Trainers/Booking and Invoicing for Trainers/New Group/ClientsViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+struct Client {
+    let name: String
+}
+
 class ClientsViewController: UIViewController {
     
     @IBOutlet weak var clientListTableView: UITableView!
     @IBOutlet weak var addClientButton: UIBarButtonItem!
+    var listOfNames: [Client] = []{
+        didSet {
+            clientListTableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,29 +29,35 @@ class ClientsViewController: UIViewController {
     }
     
     @IBAction func addClientButtonTapped(_ sender: UIBarButtonItem) {
-           let alertController = UIAlertController.init(title: "Add Client", message: "", preferredStyle: .alert)
-        alertController.addTextField { (nameTextField) in
+        let addClientToList = UIAlertController.init(title: "Add Client", message: "", preferredStyle: .alert)
+        addClientToList.addTextField { (nameTextField) in
             nameTextField.placeholder = "Name"
         }
-        alertController.addAction(.init(title: "SAVE", style: .default, handler: { (_) in
+        addClientToList.addAction(.init(title: "SAVE", style: .default, handler: { (_) in
             print("Name is saved")
+            guard let clientName = addClientToList.textFields![0].text else {return}
+            let newClient = Client(name: clientName)
+            self.listOfNames.append(newClient)
         }))
-        present(alertController, animated: true)
+        clientListTableView.reloadData()
+        present(addClientToList, animated: true)
     }
-        }
+}
 
-    
+
 
 
 extension ClientsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return listOfNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = clientListTableView.dequeueReusableCell(withIdentifier: "clientListCell", for: indexPath)
         let nameLabel = cell.contentView.viewWithTag(1)as! UILabel
-        nameLabel.text = "frikkie"
+        nameLabel.textColor = .black
+        let clientAtIndex = listOfNames[indexPath.row]
+        nameLabel.text = clientAtIndex.name
         return cell
     }
     
